7949c9fb17ba78b2267a5f741cd52fecbb922d21
Ashika Devi <129720367+ashikade@users.noreply.github.com>
2026-07-27 09:10:56 +0530

    Restore Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/qubra-clinic",
-  assetPrefix: "/qubra-clinic/",
-};
+const nextConfig = {};
 
 export default nextConfig;
